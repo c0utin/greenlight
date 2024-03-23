71ae9bc5e4174f3cebf47987577ae8c18f47a440
rafmontenegro <rafaelcouto111@gmail.com>
2024-03-23 00:12:27 -0300

    feat: add movie struct and mock data

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"greenlight.c0utin.net/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,9 +18,26 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
+
 		return
 	}
 
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Frieren",
+		Runtime:   6969,
+		Genres:    []string{"anime", "drama", "fantasy"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+
+	if err != nil {
+
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and encountered a problem and could not process your request", http.StatusInternalServerError)
+
+	}
 
 }
