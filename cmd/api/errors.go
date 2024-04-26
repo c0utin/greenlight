5c1441f858952569a36f0dc79281312d274f50cd
c0utin <rafaelcouto111@gmail.com>
2024-04-26 14:43:51 -0300

    feat: validating JSON input for movie

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -44,3 +44,8 @@ func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.
 
 	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
 }
+
+func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
+
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
+}
